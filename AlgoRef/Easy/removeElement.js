6d200a9dddb7864ee4c547019c235f61fe6cdbac
Gauthami <gauthamireddi@gmail.com>
2020-06-13 12:16:52 -0400

    Update removeElement.js
    Alternate solution

--- AlgoRef/Easy/removeElement.js
+++ AlgoRef/Easy/removeElement.js
@@ -6,3 +6,16 @@ var removeElement = function(nums, val) {
     
     return begin;
 };
+
+//another solution
+int[] input = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
+int current = input[0]; //1 in the beginning
+int value = 7
+
+for (int i = 0; i < input.length; i++) {
+    if (input[i] != value) {
+        System.out.print(" " + current);
+        current = input[i];
+    }
+}
+System.out.print(" " + current);
