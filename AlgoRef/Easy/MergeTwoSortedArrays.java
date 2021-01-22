1468117946961cac1c24977c96c81c0f9cd6ded0
Gauthami <gauthamireddi@gmail.com>
2021-01-22 16:27:34 -0500

    Update MergeTwoSortedArrays.java

--- AlgoRef/Easy/MergeTwoSortedArrays.java
+++ AlgoRef/Easy/MergeTwoSortedArrays.java
@@ -25,3 +25,8 @@ public static int[] merge(int[] a, int[] b){
         String[] array = new String[array1.length + array2.length];
         System.arraycopy(array1, 0, array, 0, array1.length);
         System.arraycopy(array2, 0, array, array1.length, array2.length);
+
+//js
+let primes = [...arr1, ...arr2];
+
+let primes = arr1.concat(arr2);
