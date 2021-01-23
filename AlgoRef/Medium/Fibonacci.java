452f52a49fdff8dd769291462ac9cd63acac8e31
Gauthami <gauthamireddi@gmail.com>
2021-01-23 17:53:22 -0500

    Update Fibonacci.java

--- AlgoRef/Medium/Fibonacci.java
+++ AlgoRef/Medium/Fibonacci.java
@@ -11,3 +11,15 @@ public class Fibonacci {
             return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
+
+var i;
+var fib = []; // Initialize array!
+
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
