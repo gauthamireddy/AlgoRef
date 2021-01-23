a7b6fcfbc2328cbea566945bb9704383f3ba42b8
Gauthami <gauthamireddi@gmail.com>
2021-01-23 07:21:38 -0500

    Update TwoSum.java

--- AlgoRef/Easy/TwoSum.java
+++ AlgoRef/Easy/TwoSum.java
@@ -11,3 +11,17 @@ public class Solution {
 
     }
 }
+
+const twoSum_On_Better = (arr, target) => {
+	let numObject = {};
+	for (let i = 0; i < arr.length; i++) {
+		let thisNum = arr[i];
+		numObject[thisNum] = i;
+	}
+	for (var i = 0; i < arr.length; i++) {
+		let diff = target - arr[i];
+		if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+			return [i, numObject[diff]];
+		}
+	}
+}
