6588199326fb140fed88c68578390caa32acb4ae
Gauthami <gauthamireddi@gmail.com>
2021-01-23 07:24:23 -0500

    Update reverseStringinWords.java

--- AlgoRef/Easy/reverseStringinWords.java
+++ AlgoRef/Easy/reverseStringinWords.java
@@ -24,3 +24,18 @@ public class Solution {
         }
     }
 }
+
+
+function reverseInPlace(str) {
+  var words = [];
+  words = str.match(/\S+/g);
+  var result = "";
+  for (var i = 0; i < words.length; i++) {
+     result += words[i].split('').reverse().join('') + " ";
+  }
+  return result
+}
+console.log(reverseInPlace("abd fhe kdj"))
+    
+    
+    //result = what I expect is "dba ehf jdk",
