7658606ce7377316a8ba2416125bb07f632cb858
Gauthami <gauthamireddi@gmail.com>
2021-01-24 05:39:51 -0500

    Update ValidParenthesis.java

--- AlgoRef/Easy/ValidParenthesis.java
+++ AlgoRef/Easy/ValidParenthesis.java
@@ -18,5 +18,34 @@ public class Solution{
 
     public static void main(String[] args){
         ValidParenthesis('{}')
+    }  
+}
+
+
+let isParenthesisMatching = (str) => {
+    let stack = [];
+
+    let open = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+
+    let closed = {
+        '}': true,
+        ']': true,
+        ')': true
     }
+
+    for (let i = 0; i < str.length; i++) {
+
+        let char = str[i];
+
+        if (open[char]) {
+            stack.push(char);
+        } else if (closed[char]) {
+            if (open[stack.pop()] !== char) return false;
+        }
+    }
+    return stack.length === 0;
 }
