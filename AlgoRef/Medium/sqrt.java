a6db859cbd86de38ef33988484f58b7929bce362
Gauthami <gauthamireddi@gmail.com>
2021-01-27 07:15:52 -0500

    Update sqrt.java

--- AlgoRef/Medium/sqrt.java
+++ AlgoRef/Medium/sqrt.java
@@ -1,16 +1,20 @@
-/*
-public class sqrt {
+function squareroot(number) {
+    for (var i = 0; i * i <= number; i++) {
+        if (i * i === number)
+            return i;
+   }
+   return number; // don't know if you should have this line in case nothing found
+}
 
-    int sqrt(int x) {
-        if (x <  2) return x;
-        int start = 0; //if start =1, may overflow
-        int end = x;
-        int mid;
-        while (start != end-1) {
-            mid = (start + end)/2;
-            if (mid == x/mid) return mid;
-            else mid < x/mid ? start = mid : end = mid;
-        }
-        return start;
+
+//alternative sqrt
+function squareroot(number) {
+    var lo = 0, hi = number;
+    while(lo <= hi) {
+         var mid = Math.floor((lo + hi) / 2);
+         if(mid * mid > number) hi = mid - 1;
+         else lo = mid + 1;
     }
-};*/
+    return hi;
+}
+
