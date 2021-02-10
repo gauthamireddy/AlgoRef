a9c335f6430c1363d3bfeedd5951a3dd7b7cd0b5
Gauthami <gauthamireddi@gmail.com>
2021-02-10 06:46:39 -0500

    Update longestPrefix.js

--- AlgoRef/Medium/longestPrefix.js
+++ AlgoRef/Medium/longestPrefix.js
@@ -4,7 +4,7 @@ let first = arr[0]
 for(let i=0; i<first.length; i++) {
   for(let j=1; j<arr.length; j++) {
     let other = arr[j];
-    if(first[i] != other[j]) return other.splice(0,i)
+    if(first[i] != other[i]) return other.slice(0,i)
   }
 }
 }
