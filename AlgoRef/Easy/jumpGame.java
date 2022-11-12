fa4fb02da195aa97361371eecd1a5949eb3954ce
Gauthami <gauthamireddi@gmail.com>
2022-11-12 13:06:16 +0530

    Update jumpGame.java

--- AlgoRef/Easy/jumpGame.java
+++ AlgoRef/Easy/jumpGame.java
@@ -4,18 +4,15 @@
 // Input: nums = [2,3,1,1,4]
 // Output: true
 // Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-    public class Solution {
-    public boolean canJump(int[] nums) {
-        if(nums == null || nums.length == 0) return false;
- 
-        int last = nums.length - 1;
-        for(int i = nums.length - 2; i >= 0; i --) {
-            if(nums[i] + i >= last) {
-                last = i;
-            }
-        }
- 
-        return last == 0;
+var canJump = function(nums) {
+    // Keep track of max distance traveled so far
+    let max = 0;
+    for(let i=0;i<nums.length;i++){
+        // The only time that max < i is when we are at 0 and we cannot move forward
+        if(i>max) return false;
+        // Move the max pointer the maximum 
+        max = Math.max(nums[i]+i,max);
     }
-}
-  
+    // If we did not get stuck at a 0 then return true
+    return true;
+}; 
