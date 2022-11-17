8edac682c05da85c4ea280cdcea1e1a42dbc421c
Gauthami <gauthamireddi@gmail.com>
2022-11-17 12:30:16 +0530

    Update MergeSortedArray.java

--- AlgoRef/Medium/MergeSortedArray.java
+++ AlgoRef/Medium/MergeSortedArray.java
@@ -1,6 +1,17 @@
 /**
  * Created by ggaddam on 8/21/2016.
  */
+
+Spread operator
+
+let arr = [...a, ...b]
+
+function mergeArray(arr1, arr2) {
+      return  arr1.concat(arr2.filter((e) => {
+                return arr1.indexOf(e) == -1
+        })
+}
+
 public class MergeSortedArray {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
             int i = m-1, j = n-1, k = m+n-1;
