e4641a39a12daf148b0c065d939ca71180341faf
Gauthami <gauthamireddi@gmail.com>
2022-11-17 07:53:36 +0530

    Update ValidPalindrome.java

--- AlgoRef/Medium/ValidPalindrome.java
+++ AlgoRef/Medium/ValidPalindrome.java
@@ -1,35 +1,31 @@
-//import java.util.Arrays;
-//
-///**
-// * Created by ggaddam on 8/21/2016.
-// */
-//public class ValidPalindrome {
-//    public boolean ValidPalindrome(String s, String t) {
-//        if (s.length() != t.length()) {
-//            return false;
-//        }
-//        int[] counter = new int[26];
-//        for (int i = 0; i < s.length(); i++) {
-//            counter[s.charAt(i) - 'a']++;
-//            counter[t.charAt(i) - 'a']--;
-//        }
-//        for (int count : counter) {
-//            if (count != 0) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//}
-//
-////o(nlogn)
-//public boolean isAnagram(String s, String t) {
-//    if (s.length() != t.length()) {
-//        return false;
-//    }
-//    char[] str1 = s.toCharArray();
-//    char[] str2 = t.toCharArray();
-//    Arrays.sort(str1);
-//    Arrays.sort(str2);
-//    return Arrays.equals(str1, str2);
-//}
+function palindrome(num) {
+    let reversedNum = '';
+    let original = num;
+    
+    while(num > 0) {
+        console.log(num)
+        reversedNum = reversedNum + num%10;
+        num=Math.floor(num/10)
+    }
+    console.log(reversedNum)
+    
+    if(reversedNum == original) return 'palindrome'
+    if(reversedNum !== original) return 'not a palindrome'
+}
+
+
+
+console.log(palindrome(323))
+  
+ 
+function palindromeString(str) {
+    let len = str.length;
+    for(let i=0; i<(len/2); i++) {
+        if(str[i] !== str[len-1-i]) {
+         return false
+        }
+    }
+    return true
+}
+
+console.log(palindromeString('madam'))
